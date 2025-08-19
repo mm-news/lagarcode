3c67b71b9b9c4e2d964475062202176d67dae132
Undefined Chang <mmtv.news.tw@gmail.com>
2025-08-19 21:16:07 +0800

    Refactor Lagrange interpolation functions to use Fraction type to kill uncertainty.

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -1,18 +1,24 @@
-def L(x:float, known_points:list[tuple[float, float]]) -> float:
-    """The main Lagrange Interpolation function. """
-    return sum([known_points[j][1]*l(j, x, known_points) for j in range(len(known_points))])
+from fractions import Fraction
 
-def l(j:int, x:float, known_points:list[tuple[float, float]]) -> float:
+def L(x:Fraction, known_points:list[tuple[Fraction, Fraction]]) -> Fraction:
+    """The main Lagrange Interpolation function. """
+    return sum([known_points[j][1]*l(j, x, known_points) for j in range(len(known_points))], Fraction(0))
+
+def l(j:int, x:Fraction, known_points:list[tuple[Fraction, Fraction]]) -> Fraction:
     """The adjustment function in Lagrange Interpolation"""
-    result = 1
+    result = Fraction(1)
     for i in range(len(known_points)):
         x_i = known_points[i][0]
         x_j = known_points[j][0]
-        result *= (x-x_i)/(x_j-x_i) if (x_j!=x_i) else 1
+        result *= (x-x_i)/(x_j-x_i) if (x_j!=x_i) else Fraction(1)
     
     return result
 
-known_points: list[tuple[float, float]] = [(1, -4), (2, 48), (3, 188), (4, 458)]
-print(L(8, known_points))
+
+known_points: list[tuple[Fraction, Fraction]] = list(zip(
+    [Fraction(i) for i in "76 97 103 114 97 32 67 111 100 101".split(" ")], [Fraction(k) for k in range(10)]))
+
+for i in range(9):
+    print(f"({10+i}, {L(Fraction(10+i), known_points)})")
 
 del known_points
